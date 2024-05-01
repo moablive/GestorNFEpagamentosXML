a162ee9bae59164f0c38f33079b86cdd966a886a
Guilherme Bonato <guilhermeferrazbonato@gmail.com>
2024-05-01 17:10:59 -0300

    Comprovantes Pagamento

--- Controllers/ComprovantesPagamentoController.cs
+++ Controllers/ComprovantesPagamentoController.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using GestorNFEpagamentosXML.Db;
 using GestorNFEpagamentosXML.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-
-namespace GestorNFEpagamentosXML.Controllers;
-
+namespace GestorNFEpagamentosXML.Controllers
+{
     [ApiController]
     [Route("api/[controller]")]
     public class ComprovantesPagamentoController : ControllerBase
@@ -23,80 +24,5 @@ namespace GestorNFEpagamentosXML.Controllers;
         {
             return await _context.ComprovantesPagamento.ToListAsync();
         }
-
-        // GET: api/ComprovantesPagamento/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ComprovantesPagamentoMODEL>> GetComprovante(int id)
-        {
-            var comprovante = await _context.ComprovantesPagamento.FindAsync(id);
-
-            if (comprovante == null)
-            {
-                return NotFound();
-            }
-
-            return comprovante;
-        }
-
-        // POST: api/ComprovantesPagamento
-        [HttpPost]
-        public async Task<ActionResult<ComprovantesPagamentoMODEL>> PostComprovante(ComprovantesPagamentoMODEL comprovante)
-        {
-            _context.ComprovantesPagamento.Add(comprovante);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetComprovante), new { id = comprovante.ID }, comprovante);
-        }
-
-        // PUT: api/ComprovantesPagamento/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutComprovante(int id, ComprovantesPagamentoMODEL comprovante)
-        {
-            if (id != comprovante.ID)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(comprovante).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ComprovanteExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // DELETE: api/ComprovantesPagamento/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteComprovante(int id)
-        {
-            var comprovante = await _context.ComprovantesPagamento.FindAsync(id);
-            if (comprovante == null)
-            {
-                return NotFound();
-            }
-
-            _context.ComprovantesPagamento.Remove(comprovante);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ComprovanteExists(int id)
-        {
-            return _context.ComprovantesPagamento.Any(e => e.ID == id);
-        }
     }
-
+}
